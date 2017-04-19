50656dd5b601086ef29da30115ee1b930cb8312c
mctag <261894551@qq.com>
2017-04-19 10:49:52 +0800

    api测试

--- aframe/build/arapi.php
+++ aframe/build/arapi.php
@@ -2,12 +2,14 @@
 /**
  * 用于AR的api
  */
+require_once './../framework/bootstrap.inc.php';
 $data = $_GET['data'];
 $data = json_decode($data);
 var_dump($data->type);
 $mydata = $data->data;
 var_dump($mydata[0]);
-
+$goods = pdo_fetchall("SELECT * FROM " . tablename('mcar_goods'));
+var_dump($goods);
 exit();
 // urldecode($data);
 // $data = json_decode(str_replace('&quot;', '"', $data), true);
