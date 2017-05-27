05971b12aaec6b80484cdc7fc46a072631201f0b
mctag <261894551@qq.com>
2017-05-27 17:40:42 +0800

    加入自主学习方案。

--- aframe/build/arapi.php
+++ aframe/build/arapi.php
@@ -21,8 +21,11 @@
 */
 
 require_once './../framework/bootstrap.inc.php';
-// $fetch=pdo_fetch("SELECT levenshtein('iossdasdasd',source) as levenshtein FROM".tablename('mcar_goods_imgInfo')."WHERE  id=3"." ORDER BY id ASC limit 1");
-// exit($fetch['levenshtein']);
+// $mystr = "0000000000000000000000000102030303040607080807060607070604020000";
+// $fetch=pdo_fetch("SELECT levenshtein('0000000000000000000101000100000101010101000000000001000000000000','0000000000000000000000010001010101010000000000010100000000000000') as levehistogram32Str,levenshtein('1001111011111111111111111111111111000000111111111111111111111111','1001110011111111111111111111111110000100111111111111111111111111') as levehistogram FROM".tablename('mcar_goods_imgInfo')."WHERE  id = 1"." ORDER BY id ASC limit 1");
+// var_dump($fetch['levehistogram32Str']);
+// exit($fetch['levehistogram']);
+
 $key='abc';//约定key
 if(isset($_GET['match'])){
 	if($_GET['match']!=$key){
@@ -45,10 +48,14 @@ if(isset($_GET['data'])){
 
 	$data=json_decode($data);
 	$mydata = $data->data;
-	$hashData = $data->hashData;
-	$histogram32Str = $data->histogram32Str;
+	$hashData = $data->hashData;//64位hash指纹图
+	$histogram32Str = $data->histogram32Str;//64位直方图字符串
+
+	
+
 	$grayData=$data->grayData;
-	$location=$data->location;
+	$location['lng']=$data->lng;
+	$location['lat']=$data->lat;
 	$source=$data->source;
 	if(isset($data->id)){
 	$url="";
@@ -74,11 +81,13 @@ if(isset($_GET['data'])){
 	
 	//4.非正常访问(无数据请求)
 	//结果：{"result": "error","code": 0}
-	$DVALUE=0.03;//分色差值
+	$DVALUE=0.04;//分色差值
 	$MGL=1;//灰度等级差值
 	$BR=0.04;//binaRate 差值
 	$RGB=0.1;//合色差值
-	$length=5;
+	// $hashDD=7;//hashData差值
+	// $histogramDD=15;//histogramDD差值
+	$length=6;
 
 
 
@@ -86,7 +95,7 @@ if(isset($_GET['data'])){
 	$result=array();
 	switch ($data->type) {
 		case 'stuff':
-			$result=saveStuffImgInfo($histogram32Str,$hashData,$mydata,$location,$data->title,$url,$grayData,$source);
+			$result=saveStuffImgInfo($histogram32Str,$hashData,$mydata,$location,$data->title,$url,$grayData,$source,0);
 			$result=json_encode($result,JSON_UNESCAPED_UNICODE);
 			exit($result);
 			break;
@@ -154,7 +163,7 @@ if(empty($result)){
 	return $re;
 }
 
-function saveStuffImgInfo($histogram32Str,$hashData,$data,$location,$title,$url,$grayData,$source)//$data数组由16个数字构成，$location为GPS经纬度，
+function saveStuffImgInfo($histogram32Str,$hashData,$data,$location,$title,$url,$grayData,$source,$mcl)//$data数组由16个数字构成，$location为GPS经纬度，$mcl判断是否来自机器学习
 	{
 		$good = pdo_fetch('SELECT id FROM ' . tablename('mcar_goods') . ' WHERE title=:title and url=:url ', array(':title' => $title, ':url' => $url));
 		if (empty($good)) {
@@ -198,7 +207,10 @@ function saveStuffImgInfo($histogram32Str,$hashData,$data,$location,$title,$url,
 
 		$imgInfoData['maxGrayLevel']=$grayData[0];
 		$imgInfoData['binaRate']=$grayData[1];
+		$imgInfoData['lng']=$location['lng'];
+		$imgInfoData['lat']=$location['lat'];
 		$imgInfoData['source']=$source;
+		$imgInfoData['mcl']=$mcl;
 		$result = pdo_insert('mcar_goods_imgInfo', $imgInfoData);
 		if ($result) {
 			$re['result']="success";
@@ -212,8 +224,7 @@ function saveStuffImgInfo($histogram32Str,$hashData,$data,$location,$title,$url,
 	}
 
 function getSimilarImgInfo($histogram32Str,$hashData,$data,$location,$grayData,$DVALUE,$MGL,$BR,$RGB,$source)//$data由16个数字构成，$location为GPS经纬度，$grayData灰度相关值
-	{
-		
+	{	
 		//location gps暂时空 todo
 		//var_dump($data[3]);
 		//test data
@@ -222,15 +233,14 @@ function getSimilarImgInfo($histogram32Str,$hashData,$data,$location,$grayData,$
 		//var_dump($data[0]);
 		
 		 //$desition=" and abs(redRate1-data1=:data1)<0.1 and abs(greenRate1-data2=:data2)<0.1 and abs(blueRate1-data3=:data3)<0.1 and abs(rgbRate1-data4=:data4)<0.2 and abs(redRate2-data5=:data5)<0.1 and abs(greenRate2-data6=:data6)<0.1 and abs(blueRate2-data7=:data7)<0.1 and abs(rgbRate2-data8=:data8)<0.2 and abs(redRate3-data9=:data9)<0.1 and abs(greenRate3-data10=:data10)<0.1 and abs(blueRate3-data11=:data11)<0.1 and abs(rgbRate3-data12:data12)<0.2 and abs(redRate-data13=:data13)<0.1 and abs(greenRate-data14=:data14)<0.1 and abs(blueRate-data15=:data15)<0.1 and abs(rgbRate-data16=:data16)<0.2";
-		if (!empty($hashData)) {
-			// $fetch=pdo_fetch("SELECT levenshtein('iossdasdasd',source) as levenshtein FROM".tablename('mcar_goods_imgInfo')."WHERE  id=3"." ORDER BY id ASC limit 1");
+		if (!empty($hashData)) {//hashData和histogram32Str一起存在或不存在
 
-			
-			$desition=" and abs(maxGrayLevel-".$grayData[0].")<".$MGL." and abs(binaRate-".$grayData[1].")<".$BR."";
+			$levehashData="levenshtein('".$hashData."',hashData) as levehashData,"."levenshtein('".$histogram32Str."',histogram32Str) as levehistogram32Str ";
+			$desition=" and levenshtein('".$hashData."',hashData)<8 and levenshtein('".$histogram32Str."',histogram32Str)<9 and abs(maxGrayLevel-".$grayData[0].")<".$MGL." and abs(binaRate-".$grayData[1].")<".$BR."";
 			$desition .=" and abs(redRate1-".$data[0].")<".$DVALUE." and abs(greenRate1-".$data[1].")<".$DVALUE." and abs(blueRate1-".$data[2].")<".$DVALUE." and abs(rgbRate1-".$data[3].")<".$RGB." and abs(redRate2-".$data[4].")<".$DVALUE." and abs(greenRate2-".$data[5].")<".$DVALUE." and abs(blueRate2-".$data[6].")<".$DVALUE."  and abs(rgbRate2-".$data[7].")<".$RGB." and abs(redRate3-".$data[8].")<".$DVALUE."  and abs(greenRate3-".$data[9].")<".$DVALUE." and abs(blueRate3-".$data[10].")<".$DVALUE." and abs(rgbRate3-".$data[11].")<".$RGB." and abs(redRate-".$data[12].")<".$DVALUE." and abs(greenRate-".$data[13].")<".$DVALUE." and abs(blueRate-".$data[14].")<".$DVALUE." and abs(rgbRate-".$data[15].")<".$RGB." and source=:source ";
 			$desition2="(abs(redRate1-".$data[0].")+abs(greenRate1-".$data[1].")+abs(blueRate1-".$data[2].")+abs(rgbRate1-".$data[3].")+abs(redRate2-".$data[4].")+abs(greenRate2-".$data[5].")+abs(blueRate2-".$data[6].")+abs(rgbRate2-".$data[7].")+abs(redRate3-".$data[8].")+abs(greenRate3-".$data[9].")+abs(blueRate3-".$data[10].")+abs(rgbRate3-".$data[11].")+abs(redRate-".$data[12].")+abs(greenRate-".$data[13].")+abs(blueRate-".$data[14].")+abs(rgbRate-".$data[15].")+abs(maxGrayLevel-".$grayData[0].")+abs(binaRate-".$grayData[1]."))";
 		 //$desition2="(abs(redRate1-".$data[0]."))";
-			$fetch=pdo_fetch("SELECT goodsid,".$desition2." as desition FROM".tablename('mcar_goods_imgInfo')."WHERE  1".$desition."ORDER BY desition ASC limit 1",array(':source'=>$source));
+			$fetch=pdo_fetch("SELECT goodsid,".$desition2." as desition,".$levehashData." FROM".tablename('mcar_goods_imgInfo')."WHERE  1".$desition."ORDER BY levehashData ASC,levehistogram32Str ASC,desition ASC limit 1",array(':source'=>$source));
 			
 		}else{
 			$desition=" and abs(maxGrayLevel-".$grayData[0].")<".$MGL." and abs(binaRate-".$grayData[1].")<".$BR."";
@@ -243,17 +253,24 @@ function getSimilarImgInfo($histogram32Str,$hashData,$data,$location,$grayData,$
 		//$fetchall=pdo_fetchall("SELECT goodsid FROM".tablename('mcar_goods_imgInfo')."WHERE 1".$desition."ORDER BY ");
 
 		 // $fetch=pdo_fetch("SELECT * FROM".tablename('mcar_goods_imginfo')."WHERE 1".$desition,array(':data1'=>$data[0],':data2'=>$data[1],':data3'=>$data[2],':data4'=>$data[3],':data5'=>$data[4],':data6'=>$data[5],':data7'=>$data[6],':data8'=>$data[7],':data9'=>$data[8],':data10'=>$data[9],':data11'=>$data[10],':data12'=>$data[11],':data13'=>$data[12],':data14'=>$data[13],':data15'=>$data[14],':data16'=>$data[15]));
-		
+		$result['histogram32Str'] = $fetch['histogram32Str'];
 		if(empty($fetch)){
 		 $result['result'] = 'fail';
 		 $result['code']=3;
 		 return $result;
 		}else {
+			$total = pdo_fetchcolumn("SELECT COUNT(*) FROM " . tablename('mcar_goods_imgInfo') . " WHERE goodsid=:goodsid" ,array('goodsid' => $fetch['goodsid']));
+			
 			$goods=pdo_fetch("SELECT * FROM".tablename('mcar_goods')." WHERE id=:id",array(':id'=> $fetch['goodsid']));
 			$result['result'] = 'success';
 			$result['success'] = 'true';
 			$result['title'] = $goods['title'];		
 			$result['url'] = $goods['url'];
+
+			if ($total<500) {
+				saveStuffImgInfo($histogram32Str,$hashData,$data,$location,$goods['title'],$goods['url'],$grayData,$source,1);
+			}
+			
 			return $result;
 		} 
 	}//返回$result
